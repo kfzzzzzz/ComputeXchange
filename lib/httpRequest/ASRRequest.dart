d615f42c5329c5477f7b1265ef138f6b0dd01140
KFZZZZZZ\31065 <310654959@qq.com>
2023-04-04 01:28:36 +0800

    哎

--- lib/httpRequest/ASRRequest.dart
+++ lib/httpRequest/ASRRequest.dart
@@ -5,10 +5,11 @@ import 'package:uuid/uuid.dart';
 import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
+import 'package:flutter/services.dart' show rootBundle;
 
-const demo = '';
-const demodemo = '';
-const demodemodemo = '';
+const demo = 'https://openapi.youdao.com/asrapi';
+const demodemo = '3b79db3846bd3ae7';
+const demodemodemo = 'OBQrU64Knaxz4zgWzdkFBN6LRSDwAIif';
 
 String truncate(String q) {
   if (q == null) {
@@ -18,6 +19,20 @@ String truncate(String q) {
   return size <= 20
       ? q
       : '${q.substring(0, 10)}$size${q.substring(size - 10, size)}';
+}
+
+String computeHash(String input, Hash hash) {
+  List<int> inputBytes = utf8.encode(input);
+  List<int> hashedBytes = hash.convert(inputBytes).bytes;
+  return hashedBytes
+      .map((byte) => byte.toRadixString(16).padLeft(2, '0'))
+      .join('');
+}
+
+String encodeSHA256(String input) {
+  var bytes = utf8.encode(input); //将字符串转为字节数组
+  var digest = sha256.convert(bytes); //使用sha256方法进行编码
+  return digest.toString(); //将编码后的字节数组转为字符串
 }
 
 String encrypt(String signStr) {
@@ -31,22 +46,34 @@ Future<http.Response> doRequest(Map<String, String> data) {
 }
 
 Future<void> ASRRequest() async {
-  const audioFilePath = '音频的路径';
-  const langType = 'zh-CHS';
-  final file = File(audioFilePath);
-  final fileExtension = path.extension(audioFilePath);
-  if (fileExtension != '.wav') {
-    print('不支持的音频类型');
-    return;
-  }
-
-  final bytes = await file.readAsBytes();
+  final file = await rootBundle.load('wav/testwav.wav');
+  final bytes = file.buffer.asUint8List();
   final q = base64.encode(bytes);
 
-  final curTime = DateTime.now().millisecondsSinceEpoch.toString();
+  // final file = File("C:/Users/31065/Desktop/testwav.wav");
+  // // final fileExtension = path.extension(file);
+  // // if (fileExtension != '.wav') {s
+  // //   print('不支持的音频类型');
+  // //   return;
+  // // }
+  // final bytes = await file.readAsBytes();
+  // final q = base64.encode(bytes);
+
+  const langType = 'zh-CHS';
+
+  final curtime =
+      (DateTime.now().millisecondsSinceEpoch / 1000).floor().toString();
+  print(curtime);
+  // const hours8InMilliseconds = 8 * 60 * 60;
+  //final curTime = cur - hours8InMilliseconds;
+  // print(curTime);
   final salt = const Uuid().v1();
-  final signStr = '$demodemo${truncate(q)}$salt$curTime$demodemodemo';
-  final sign = encrypt(signStr);
+  String signStr = demodemo + truncate(q) + salt + curtime + demodemodemo;
+  String sign = encodeSHA256(signStr);
+  print(signStr);
+  print(sign);
+  // final signStr = '$demodemo${truncate(q)}$salt$curtime$demodemodemo';
+  // final sign = encrypt(signStr);
 
   final data = <String, String>{
     'appKey': demodemo,
@@ -62,5 +89,7 @@ Future<void> ASRRequest() async {
   };
 
   final response = await doRequest(data);
+  final serverTime = response.headers['date'];
+  print('服务器时间：$serverTime');
   print(response.body);
 }
--- lib/textToImage/bloc/text_to_image_bloc.dart
+++ lib/textToImage/bloc/text_to_image_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:compute_xchange/httpRequest/ASRRequest.dart';
 import 'package:compute_xchange/httpRequest/stableDiffusionRequest.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
@@ -9,6 +10,7 @@ part 'text_to_image_state.dart';
 class TextToImageBloc extends Bloc<TextToImageEvent, TextToImageState> {
   TextToImageBloc() : super(TextToImageInitial()) {
     on<TextToImageLoadEvent>((event, emit) async {
+      ASRRequest();
       Future<Image> resultList = sendPostRequest(event.description);
       emit(TextToImageLoading());
       await resultList.then((Image result) {
